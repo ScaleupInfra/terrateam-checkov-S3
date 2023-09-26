88604cde8d6e3680d746ab773c7c8ffee4216f07
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-27 01:54:44 +0530

    public access blocked

--- AWS-S3/main.tf
+++ AWS-S3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "infraSity" {
-  bucket = lower("SIDVJSINGH")
+  bucket = lower("infraSity")
   acl = "private"
   versioning {
     enabled = true
@@ -8,3 +8,10 @@ resource "aws_s3_bucket" "infraSity" {
     name = "S3 bucket"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "public_block" {
+   bucket = aws_s3_bucket.infraSity.id
+
+   block_public_acls   = true
+   block_public_policy = true
+ }
