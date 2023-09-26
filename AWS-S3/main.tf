4fb23f046eea47baa335d5cc1edb7d668618ef4c
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-26 19:31:02 +0530

    tags added

--- AWS-S3/main.tf
+++ AWS-S3/main.tf
@@ -1,7 +1,10 @@
-resource "aws_s3_bucket" "infraSity" {
+resource "aws_s3_bucket" "infrasity" {
   bucket = lower("infraSity")
   acl = "private"
   versioning {
     enabled = true
+  }
+  tags = {
+    name = "S3"
   }
 }
