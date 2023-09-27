c735bbaca72ebbb921664e1d6852366bf0bb9a2c
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-27 18:27:27 +0530

    main updated

--- AWS-S3/main.tf
+++ AWS-S3/main.tf
@@ -8,10 +8,3 @@ resource "aws_s3_bucket" "infraSity" {
     name = "S3 bucket"
   }
 }
-
-resource "aws_s3_bucket_public_access_block" "public_block" {
-   bucket = aws_s3_bucket.infraSity.id
-
-   block_public_acls   = true
-   block_public_policy = true
- }
