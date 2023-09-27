f700956f406fb1be856b7e3214f786d21dd73439
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-28 03:24:13 +0530

    OPA runner

--- AWS-S3/main.tf
+++ AWS-S3/main.tf
@@ -8,14 +8,3 @@ resource "aws_s3_bucket" "infraSity" {
     name = "S3 bucket"
   }
 }
-
-resource "aws_s3_bucket" "sidvjsingh" {
-  bucket = lower("sidvjsingh")
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-  tags ={
-    name = "S3 bucket2"
-  }
-}
